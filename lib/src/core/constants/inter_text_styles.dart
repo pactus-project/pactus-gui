1fc6e83d87f5f071736f8ee63ae63e38cd1b6604
PouriaMoradi021 <pooriya.mrd1378@gmail.com>
2024-12-06 15:02:56 +0330

    feat(app): add `inter text styles` to app #34

---
+++ lib/src/core/constants/inter_text_styles.dart
@@ -0,0 +1,283 @@
+import 'package:flutter/material.dart' show TextStyle, FontWeight;
+
+class InterTextStyles {
+  InterTextStyles._();
+
+  static const interThin = 'Inter-Thin';
+  static const interExtraLight = 'Inter-ExtraLight';
+  static const interLight = 'Inter-Light';
+  static const interRegular = 'Inter-Regular';
+  static const interMedium = 'Inter-Medium';
+  static const interSemiBold = 'Inter-SemiBold';
+  static const interBold = 'Inter-Bold';
+  static const interExtraBold = 'Inter-ExtraBold';
+
+
+
+  // InterExtraLight Text Styles
+  static const TextStyle extraLight4 = TextStyle(
+    fontFamily: interExtraLight,
+    fontSize: 4,
+    fontWeight: FontWeight.w200,
+    height: 2.5, // Equivalent to 10/4
+  );
+
+  static const TextStyle extraLight6 = TextStyle(
+    fontFamily: interExtraLight,
+    fontSize: 6,
+    fontWeight: FontWeight.w200,
+    height: 2.67, // Equivalent to 16/6
+  );
+
+  static const TextStyle extraLight14 = TextStyle(
+    fontFamily: interExtraLight,
+    fontSize: 14,
+    fontWeight: FontWeight.w200,
+    height: 2.0, // Equivalent to 28/14
+  );
+
+  static const TextStyle extraLight15 = TextStyle(
+    fontFamily: interExtraLight,
+    fontSize: 15,
+    fontWeight: FontWeight.w200,
+    height: 1.6, // Equivalent to 24/15
+  );
+
+  static const TextStyle extraLight30 = TextStyle(
+    fontFamily: interExtraLight,
+    fontSize: 30,
+    fontWeight: FontWeight.w200,
+    height: 1.47, // Equivalent to 44/30
+  );
+
+  // InterLight Text Styles
+  static const TextStyle light9 = TextStyle(
+    fontFamily: interLight,
+    fontSize: 9,
+    fontWeight: FontWeight.w300,
+    height: 1.67, // Equivalent to 15/9
+  );
+
+  static const TextStyle light10 = TextStyle(
+    fontFamily: interLight,
+    fontSize: 10,
+    fontWeight: FontWeight.w300,
+    height: 2.0, // Equivalent to 20/10
+  );
+
+  static const TextStyle light14 = TextStyle(
+    fontFamily: interLight,
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+    height: 1.43, // Equivalent to 20/14
+  );
+
+  static const TextStyle light15 = TextStyle(
+    fontFamily: interLight,
+    fontSize: 15,
+    fontWeight: FontWeight.w300,
+    height: 1.6, // Equivalent to 24/15
+  );
+
+  static const TextStyle light18 = TextStyle(
+    fontFamily: interLight,
+    fontSize: 18,
+    fontWeight: FontWeight.w300,
+    height: 1.33, // Equivalent to 24/18
+  );
+
+
+  // InterRegular Text Styles
+  static const TextStyle regular8 = TextStyle(
+    fontFamily: interRegular,
+    fontSize: 8,
+    fontWeight: FontWeight.w400,
+    height: 2.5, // Equivalent to 20/8
+  );
+
+  static const TextStyle regular10H1 = TextStyle(
+    fontFamily: interRegular,
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    height: 1.6, // Equivalent to 16/10
+  );
+
+  static const TextStyle regular10H2 = TextStyle(
+    fontFamily: interRegular,
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    height: 2.0, // Equivalent to 20/10
+  );
+
+  static const TextStyle regular11H1 = TextStyle(
+    fontFamily: interRegular,
+    fontSize: 11,
+    fontWeight: FontWeight.w400,
+    height: 1.64, // Equivalent to 18/11
+  );
+
+  static const TextStyle regular11H2 = TextStyle(
+    fontFamily: interRegular,
+    fontSize: 11,
+    fontWeight: FontWeight.w400,
+    height: 1.82, // Equivalent to 20/11
+  );
+
+  static const TextStyle regular12H1 = TextStyle(
+    fontFamily: interRegular,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.33, // Equivalent to 16/12
+  );
+
+  static const TextStyle regular12H2 = TextStyle(
+    fontFamily: interRegular,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.67, // Equivalent to 20/12
+  );
+
+  static const TextStyle regular13 = TextStyle(
+    fontFamily: interRegular,
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    height: 1.92, // Equivalent to 25/13
+  );
+
+  static const TextStyle regular14 = TextStyle(
+    fontFamily: interRegular,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.43, // Equivalent to 20/14
+  );
+
+  // InterMedium Text Styles
+  static const TextStyle medium11 = TextStyle(
+    fontFamily: interMedium,
+    fontSize: 11,
+    fontWeight: FontWeight.w500,
+    height: 1.82, // Equivalent to 20/11
+  );
+
+  static const TextStyle medium12H1 = TextStyle(
+    fontFamily: interMedium,
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    height: 1.42, // Equivalent to 17/12
+  );
+
+  static const TextStyle medium12H2 = TextStyle(
+    fontFamily: interMedium,
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    height: 1.67, // Equivalent to 20/12
+  );
+
+  static const TextStyle medium13 = TextStyle(
+    fontFamily: interMedium,
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+    height: 1.23, // Equivalent to 16/13
+  );
+
+  static const TextStyle medium14 = TextStyle(
+    fontFamily: interMedium,
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 1.43, // Equivalent to 20/14
+  );
+
+  // InterSemiBold Text Styles
+  static const TextStyle semiBold7 = TextStyle(
+    fontFamily: interSemiBold,
+    fontSize: 7,
+    fontWeight: FontWeight.w600,
+    height: 2.86, // Equivalent to 20/7
+  );
+
+  static const TextStyle semiBold10 = TextStyle(
+    fontFamily: interSemiBold,
+    fontSize: 10,
+    fontWeight: FontWeight.w600,
+    height: 2.0, // Equivalent to 20/10
+  );
+
+  static const TextStyle semiBold14H1 = TextStyle(
+    fontFamily: interSemiBold,
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    height: 1.14, // Equivalent to 16/14
+  );
+
+  static const TextStyle semiBold14H2 = TextStyle(
+    fontFamily: interSemiBold,
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    height: 1.43, // Equivalent to 20/14
+  );
+
+  static const TextStyle semiBold14H3 = TextStyle(
+    fontFamily: interSemiBold,
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    height: 1.71, // Equivalent to 24/14
+  );
+
+  static const TextStyle semiBold14H4 = TextStyle(
+    fontFamily: interSemiBold,
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    height: 2.0, // Equivalent to 28/14
+  );
+
+  static const TextStyle semiBold16 = TextStyle(
+    fontFamily: interSemiBold,
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    height: 1.5, // Equivalent to 24/16
+  );
+
+  static const TextStyle semiBold24 = TextStyle(
+    fontFamily: interSemiBold,
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    height: 1.33, // Equivalent to 32/24
+  );
+
+  // InterBold Text Styles
+  static const TextStyle bold9 = TextStyle(
+    fontFamily: interBold,
+    fontSize: 9,
+    fontWeight: FontWeight.w700,
+    height: 2.22, // Equivalent to 20/9
+  );
+
+  static const TextStyle bold10 = TextStyle(
+    fontFamily: interBold,
+    fontSize: 10,
+    fontWeight: FontWeight.w700,
+    height: 2.0, // Equivalent to 20/10
+  );
+
+  static const TextStyle bold12 = TextStyle(
+    fontFamily: interBold,
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    height: 1.67, // Equivalent to 20/12
+  );
+
+  static const TextStyle bold13 = TextStyle(
+    fontFamily: interBold,
+    fontSize: 13,
+    fontWeight: FontWeight.w700,
+    height: 1.23, // Equivalent to 16/13
+  );
+
+  static const TextStyle bold14 = TextStyle(
+    fontFamily: interBold,
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    height: 2.0, // Equivalent to 28/14
+  );
+}
+
