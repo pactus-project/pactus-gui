a8e61252970a48517068a983f69b0fd16d650cdb
esmaeil.ahmadipour <ahmadipour1402@gmail.com>
2025-01-06 21:01:11 +0330

    fix(features:password): fix buggy behavior of PasswordBox in linux

--- lib/src/features/password/presentation/widgets/master_password_text_form_field_widget.dart
+++ lib/src/features/password/presentation/widgets/master_password_text_form_field_widget.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gui/src/core/common/colors/app_colors.dart';
-import 'package:gui/src/core/constants/inter_text_styles.dart';
+import 'package:gui/src/core/common/widgets/text_field_wrapper.dart';
 import 'package:gui/src/features/password/presentation/cubits/master_password_handler_cubit.dart';
 
 class MasterPasswordTextFormFieldWidget extends StatefulWidget {
@@ -25,42 +24,8 @@ class _MasterPasswordTextFormFieldWidgetState
           return SizedBox(
             height: 32,
             width: 280,
-            child: TextFormField(
-              controller: passwordController,
-              obscureText: isObscured,
-              style: InterTextStyles.regular14
-                  .copyWith(color: AppColors.primaryGrey),
-              decoration: InputDecoration(
-                enabledBorder: const UnderlineInputBorder(
-                  borderSide:
-                      BorderSide(color: AppColors.compoundRest, width: 2),
-                ),
-                focusedBorder: const UnderlineInputBorder(
-                  borderSide:
-                      BorderSide(color: AppColors.compoundRest, width: 2),
-                ),
-                contentPadding: const EdgeInsetsDirectional.symmetric(
-                    vertical: 5, horizontal: 12),
-                border: const OutlineInputBorder(),
-                suffixIcon: Padding(
-                  padding: const EdgeInsets.only(
-                    bottom: 9,
-                  ),
-                  child: IconButton(
-                    icon: Icon(
-                      isObscured ? Icons.visibility_off : Icons.visibility,
-                    ),
-                    onPressed: () {
-                      context
-                          .read<MasterPasswordHandlerCubit>()
-                          .toggleVisibility();
-                    },
-                  ),
-                ),
-              ),
-              validator: (value) {
-                return null;
-              },
+            child: TextFieldWrapper(
+              child: PasswordBox(),
             ),
           );
         },
