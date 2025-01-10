63f63201b04b229d93c045d1f01e49745404802d
esmaeil.ahmadipour <ahmadipour1402@gmail.com>
2025-01-10 09:02:50 +0330

    docs: update documents of `ThemeSwitcher` class according to new changes

--- lib/src/core/common/widgets/theme_switcher.dart
+++ lib/src/core/common/widgets/theme_switcher.dart
@@ -5,12 +5,37 @@ import 'package:gui/src/core/common/colors/app_colors.dart';
 import 'package:gui/src/core/utils/gen/assets/assets.gen.dart';
 import 'package:gui/src/features/main/theme/bloc/theme_bloc.dart';
 
-/// ### [ThemeSwitcher] Documentation
-/// A widget that toggles between light and dark themes using a animated switch.
+/// ### [ThemeSwitcher] Widget documentation
 ///
-/// - Uses `Theme.of(context)` to determine the current theme.
-/// - Displays different icons for light and dark modes with animations.
-/// - Provides a switch that triggers a theme change by `ThemeBloc` when tapped.
+/// A custom widget that toggles between light and dark themes using an
+/// animated switch. It provides a smooth user experience with visually
+/// engaging transitions between the themes.
+///
+/// #### Key Features:
+/// - Utilizes `Theme.of(context)` to check the current theme mode.
+/// - Displays different icons for light and dark themes with animated
+///   opacity.
+/// - Includes an interactive switch with animations for theme toggling.
+/// - Relies on `ThemeBloc` for state management and theme updates.
+///
+/// #### Widget Structure:
+/// 1. **Icons**: Displays a moon icon for the dark theme and a sun icon for
+///    the light theme with opacity animations.
+/// 2. **Switch**: An interactive container that acts as a theme toggle
+///    with animated alignment and styling.
+///
+/// #### Interaction:
+/// - On tapping the switch, a `ThemeChanged` event is dispatched to the
+///   `ThemeBloc` to update the theme mode.
+///
+/// #### Animations:
+/// - Icons fade in and out with an `AnimatedOpacity` widget.
+/// - Switch thumb aligns to the left or right using `AnimatedAlign`.
+/// - Switch background adjusts its appearance with an `AnimatedContainer`.
+///
+/// #### Example:
+/// ```dart
+/// ThemeSwitcher();
 ///
 class ThemeSwitcher extends StatelessWidget {
   const ThemeSwitcher({super.key});
@@ -21,9 +46,11 @@ class ThemeSwitcher extends StatelessWidget {
       builder: (context, themeState) {
         const duration = Duration(milliseconds: 200);
         final isLightTheme = themeState.themeMode == ThemeMode.light;
+
         return Row(
           mainAxisSize: MainAxisSize.min,
           children: [
+            // Dark mode icon
             AnimatedOpacity(
               curve: Curves.easeIn,
               opacity: isLightTheme ? 0.0 : 1.0,
@@ -42,9 +69,9 @@ class ThemeSwitcher extends StatelessWidget {
                     );
               },
               child: AnimatedContainer(
-                margin: EdgeInsets.symmetric(horizontal: 4),
-                width: 40, // Total width of the switch
-                height: 20, // Total height of the switch
+                margin: const EdgeInsets.symmetric(horizontal: 4),
+                width: 40,
+                height: 20,
                 duration: duration * 2,
                 decoration: BoxDecoration(
                   color: Colors.transparent,
@@ -56,13 +83,13 @@ class ThemeSwitcher extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 3),
                   child: AnimatedAlign(
-                    duration: Duration(milliseconds: 100),
+                    duration: const Duration(milliseconds: 100),
                     alignment: isLightTheme
                         ? Alignment.centerRight
                         : Alignment.centerLeft,
                     child: Container(
-                      width: 14, // Thumb width
-                      height: 14, // Thumb height
+                      width: 14,
+                      height: 14,
                       decoration: BoxDecoration(
                         color: AppColors.primaryDark,
                         borderRadius: BorderRadius.circular(7),
@@ -72,7 +99,7 @@ class ThemeSwitcher extends StatelessWidget {
                 ),
               ),
             ),
-            // Sun icon (right side)
+            // Light mode icon
             AnimatedOpacity(
               curve: Curves.easeIn,
               opacity: isLightTheme ? 1.0 : 0.0,
