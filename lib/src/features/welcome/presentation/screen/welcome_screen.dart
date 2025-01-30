da40084883d7143e373eca416ec52ce0dd1b4fc1
PouriaMoradi021 <pooriya.mrd1378@gmail.com>
2025-01-30 15:32:24 +0330

    docs: add/update documents according to mark down standards

--- lib/src/features/welcome/presentation/screen/welcome_screen.dart
+++ lib/src/features/welcome/presentation/screen/welcome_screen.dart
@@ -8,6 +8,44 @@ import 'package:gui/src/core/utils/assets/assets.gen.dart';
 import 'package:gui/src/core/utils/gen/localization/locale_keys.dart';
 import 'package:gui/src/features/main/language/core/localization_extension.dart';
 import 'package:pactus_gui_widgetbook/app_styles.dart';
+/// ### [WelcomeScreen] Widget Documentation
+///
+/// The `WelcomeScreen` is a stateless widget that serves as the introductory
+/// screen of the application.
+/// It presents a welcoming message, a description, an image, and a button to
+/// proceed further.
+///
+/// ---
+///
+/// ### Key Features
+///
+/// 1. **NavigationView Layout:**
+///    - The screen is wrapped inside a `NavigationView`, ensuring a consistent
+///    UI structure.
+///
+/// 2. **Responsive Design:**
+///    - Uses a `SingleChildScrollView` to ensure content is scrollable if
+///    needed.
+///    - Elements are spaced using `Gap` widgets to maintain a structured
+///    layout.
+///
+/// 3. **Localized Text Support:**
+///    - The title and description are translated using
+///    `context.tr(LocaleKeys.key)`, ensuring multilingual support.
+///
+/// 4. **Custom Button:**
+///    - A `CustomFilledButton` is used for navigation.
+///    - Clicking the button redirects the user to the next screen
+///    (`AppRoute.initializeMode.name`).
+///
+/// ---
+///
+/// ### Expected Behavior
+/// - Displays an image, welcome text, description, and a button.
+/// - Ensures text and button are localized for different languages.
+/// - Clicking the button navigates the user to the next screen.
+/// - Uses a scrollable layout to accommodate different screen sizes.
+
 ///to-do #68: add components for this screen after designing process complete
 /// by Pouria
 class WelcomeScreen extends StatelessWidget {
