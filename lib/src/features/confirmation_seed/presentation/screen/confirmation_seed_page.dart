e335a1bf3ef4a2682b019e189c5088186d26bef6
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2025-02-15 00:17:31 +0330

    update(features:confirmationSeed): update ui of `chip text box` in confirmation seed screen

--- lib/src/features/confirmation_seed/presentation/screen/confirmation_seed_page.dart
+++ lib/src/features/confirmation_seed/presentation/screen/confirmation_seed_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gui/src/features/confirmation_seed/data/models/seed_item_data.dart';
 import 'package:gui/src/features/confirmation_seed/presentation/bloc/confirmation_seed_cubit.dart';
 import 'package:gui/src/features/main/navigation_pan_cubit/presentation/cubits/navigation_pan_cubit.dart';
+import 'package:pactus_gui_widgetbook/core.dart';
+import 'package:pactus_gui_widgetbook/widgets.dart';
 
 class ConfirmationSeedPage extends StatelessWidget {
   const ConfirmationSeedPage({super.key});
@@ -12,184 +14,137 @@ class ConfirmationSeedPage extends StatelessWidget {
     return BlocBuilder<NavigationPaneCubit, int>(
       builder: (context, selectedIndex) {
         return NavigationView(
-          content:/* Column(
-            children: [*/
-              Column(
-                children: [
-                  Expanded(
-                    child: BlocBuilder<ConfirmationSeedCubit,
-                        ConfirmationSeedState>(
-                      builder: (context, state) {
-                        if (state.words.isEmpty) {
-                          return const Center(child: ProgressRing());
-                        }
+          content: Column(
+            children: [
+              Expanded(
+                child:
+                    BlocBuilder<ConfirmationSeedCubit, ConfirmationSeedState>(
+                  builder: (context, state) {
+                    if (state.words.isEmpty) {
+                      return const Center(child: ProgressRing());
+                    }
 
-                        return Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: LayoutBuilder(
-                            builder: (context, constraints) {
-                              final crossAxisCount =
-                                  (constraints.maxWidth / 160)
-                                      .floor()
-                                      .clamp(2, 6);
+                    return Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: LayoutBuilder(
+                        builder: (context, constraints) {
+                          final crossAxisCount =
+                              (constraints.maxWidth / 160).floor().clamp(2, 6);
 
-                              return GridView.builder(
-                                gridDelegate:
-                                    SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: crossAxisCount,
-                                  crossAxisSpacing: 12,
-                                  mainAxisSpacing: 12,
-                                  mainAxisExtent: 48,
-                                ),
-                                itemCount: state.words.length,
-                                padding: EdgeInsets.zero,
-                                itemBuilder: (context, index) {
-                                  final item = state.words[index];
+                          return GridView.builder(
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: crossAxisCount,
+                              crossAxisSpacing: 12,
+                              mainAxisSpacing: 12,
+                              mainAxisExtent: 48,
+                            ),
+                            itemCount: state.words.length,
+                            padding: EdgeInsets.zero,
+                            itemBuilder: (context, index) {
+                              final item = state.words[index];
+                              final wordID = index + 1;
+                              final chipTextMode = state
+                                  .validationResults[index]
+                                  .detectChipTextModeOnBoolean();
 
-                                  if (!item.isNeedConfirmation) {
-                                    return Container(
-                                      alignment: Alignment.center,
-                                      decoration: BoxDecoration(
-                                        color: FluentTheme.of(context)
-                                            .micaBackgroundColor,
-                                        borderRadius:
-                                            BorderRadius.circular(20),
-                                      ),
-                                      padding: const EdgeInsets.symmetric(
-                                        vertical: 10,
-                                      ),
-                                      child: Text(
-                                        item.word,
-                                        textAlign: TextAlign.center,
-                                        style: FluentTheme.of(context)
-                                            .typography
-                                            .bodyStrong!
-                                            .copyWith(color: Colors.black),
-                                      ),
-                                    );
-                                  }
+                              if (!item.isNeedConfirmation) {
+                                return ChipTextBox(
+                                  prefixText: '$wordID.',
+                                  isReadOnly: true,
+                                  chipTextMode: ChipTextMode.normal,
+                                  placeholder: item.word,
+                                  onChanged: (value) {
+                                    context
+                                        .read<ConfirmationSeedCubit>()
+                                        .updateValidation(index, value);
+                                  },
+                                );
+                              }
 
-                                  final isCorrect =
-                                      state.validationResults[index];
-                                  final color = (isCorrect ?? false)
-                                      ? Colors.green
-                                      : Colors.red;
-
-                                  return ChipTextBox(
-                                    backgroundColor: color,
-                                    placeholder: '',
-                                    onChanged: (value) {
-                                      context
-                                          .read<ConfirmationSeedCubit>()
-                                          .updateValidation(index, value);
-                                    },
-                                  );
+                              return ChipTextBox(
+                                prefixText: '$wordID.',
+                                chipTextMode: chipTextMode,
+                                onChanged: (value) {
+                                  context
+                                      .read<ConfirmationSeedCubit>()
+                                      .updateValidation(index, value);
                                 },
                               );
                             },
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: BlocBuilder<ConfirmationSeedCubit,
-                        ConfirmationSeedState>(
-                      builder: (context, state) {
-                        return Button(
-                          child: Text(
-                            'Check',
-                            style: TextStyle(
-                              color: Colors.black,
-                            ),
-                          ),
-                          onPressed: () {
-                            final isValidated = context
-                                .read<ConfirmationSeedCubit>()
-                                .isAllValid();
-                            final text = isValidated ? 'Valid' : 'Invalid';
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child:
+                    BlocBuilder<ConfirmationSeedCubit, ConfirmationSeedState>(
+                  builder: (context, state) {
+                    return Button(
+                      child: Text(
+                        'Check',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      onPressed: () {
+                        final isValidated =
+                            context.read<ConfirmationSeedCubit>().isAllValid();
+                        final text = isValidated ? 'Valid' : 'Invalid';
 
-                            displayInfoBar(
-                              context,
-                              builder: (context, close) {
-                                return InfoBar(
-                                  title: Text(
-                                    text,
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  severity: isValidated
-                                      ? InfoBarSeverity.success
-                                      : InfoBarSeverity.error,
-                                );
-                              },
+                        displayInfoBar(
+                          context,
+                          builder: (context, close) {
+                            return InfoBar(
+                              title: Text(
+                                text,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                              severity: isValidated
+                                  ? InfoBarSeverity.success
+                                  : InfoBarSeverity.error,
                             );
                           },
                         );
                       },
+                    );
+                  },
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (selectedIndex > 0)
+                    Button(
+                      child: const Text('Previous'),
+                      onPressed: () {
+                        context
+                            .read<NavigationPaneCubit>()
+                            .setSelectedIndex(selectedIndex - 1);
+                      },
                     ),
-                  ),
+                  const SizedBox(width: 20),
+                  if (selectedIndex < 6)
+                    Button(
+                      child: const Text('Next'),
+                      onPressed: () {
+                        context
+                            .read<NavigationPaneCubit>()
+                            .setSelectedIndex(selectedIndex + 1);
+                      },
+                    ),
                 ],
               ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              //     if (selectedIndex > 0)
-              //       Button(
-              //         child: const Text('Previous'),
-              //         onPressed: () {
-              //           context
-              //               .read<NavigationPaneCubit>()
-              //               .setSelectedIndex(selectedIndex - 1);
-              //         },
-              //       ),
-              //     const SizedBox(width: 20),
-              //     if (selectedIndex < 6)
-              //       Button(
-              //         child: const Text('Next'),
-              //         onPressed: () {
-              //           context
-              //               .read<NavigationPaneCubit>()
-              //               .setSelectedIndex(selectedIndex + 1);
-              //         },
-              //       ),
-              //   ],
-              // ),
-      /*      ],
-          ),*/
+            ],
+          ),
         );
       },
-    );
-  }
-}
-
-class ChipTextBox extends StatelessWidget {
-  const ChipTextBox({
-    super.key,
-    required this.backgroundColor,
-    required this.onChanged,
-    this.placeholder,
-  });
-  final Color backgroundColor;
-  final String? placeholder;
-  final ValueChanged<String> onChanged;
-  @override
-  Widget build(BuildContext context) {
-    return ExcludeSemantics(
-      child: TextBox(
-        padding: EdgeInsets.zero,
-        placeholder: placeholder,
-        decoration: WidgetStateProperty.all(
-          BoxDecoration(
-            borderRadius: BorderRadius.circular(64),
-            color: backgroundColor,
-          ),
-        ),
-        textAlign: TextAlign.center,
-        onChanged: onChanged,
-      ),
     );
   }
 }
