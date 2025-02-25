7c937322f5f9bbca21d349851b968b2dddbced99
Henry Nguyen <hoanguit@gmail.com>
2025-02-25 18:20:59 +0700

    update: add the workflow for check password

--- lib/src/features/finish/presentation/screen/finish_page.dart
+++ lib/src/features/finish/presentation/screen/finish_page.dart
@@ -2,16 +2,45 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gui/src/core/router/route_name.dart';
+import 'package:gui/src/core/utils/daemon_manager/node_config_data.dart';
+import 'package:gui/src/core/utils/storage_utils.dart';
 import 'package:gui/src/features/main/language/core/localization_extension.dart';
 import 'package:pactus_gui_widgetbook/app_styles.dart';
-
 import '../../../../core/common/widgets/custom_filled_button.dart';
 import '../../../../core/utils/gen/assets/assets.gen.dart';
 import '../../../../core/utils/gen/localization/locale_keys.dart';
 import '../../../../core/utils/string_extension.dart';
 
-class FinishPage extends StatelessWidget {
+class FinishPage extends StatefulWidget {
   const FinishPage({super.key});
+
+  @override
+  State<FinishPage> createState() => _FinishPageState();
+}
+
+class _FinishPageState extends State<FinishPage> {
+  Future<void> _handleFinish(BuildContext context) async {
+    await StorageUtils.setInstallationFinished(isFinished: true);
+    if (!context.mounted) {
+      return;
+    }
+
+    if (NodeConfigData.instance.password.isNotEmpty) {
+      await StorageUtils.savePasswordIfNotEmpty(
+        NodeConfigData.instance.password,
+      );
+      if (!context.mounted) {
+        return;
+      }
+      context.goNamed(AppRoute.dashboard.name);
+    } else {
+      await StorageUtils.savePasswordIfNotEmpty('');
+      if (!context.mounted) {
+        return;
+      }
+      context.goNamed(AppRoute.password.name);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -95,7 +124,7 @@ class FinishPage extends StatelessWidget {
         CustomFilledButton(
           text: LocaleKeys.go_to_dashboard,
           onPressed: () {
-            context.goNamed(AppRoute.password.name);
+            _handleFinish(context);
           },
           style: ButtonStyle(
             padding: WidgetStateProperty.all<EdgeInsetsDirectional?>(
