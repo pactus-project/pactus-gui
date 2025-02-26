cf03a28c0c5c3f9d3902aa340fa0f80db6d0331c
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2025-02-26 10:06:43 +0330

    update: update generated `assets.gen.dart` file

--- lib/src/core/utils/gen/assets/assets.gen.dart
+++ lib/src/core/utils/gen/assets/assets.gen.dart
@@ -91,7 +91,7 @@ class $AssetsImagesGen {
 }
 
 class Assets {
-  Assets._();
+  const Assets._();
 
   static const $AssetsIconsGen icons = $AssetsIconsGen();
   static const $AssetsImagesGen images = $AssetsImagesGen();
@@ -130,7 +130,7 @@ class AssetGenImage {
     bool gaplessPlayback = true,
     bool isAntiAlias = false,
     String? package,
-    FilterQuality filterQuality = FilterQuality.low,
+    FilterQuality filterQuality = FilterQuality.medium,
     int? cacheWidth,
     int? cacheHeight,
   }) {
