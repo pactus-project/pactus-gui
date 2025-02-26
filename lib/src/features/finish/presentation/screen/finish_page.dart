3ec5e4c1bd6625d211c882c4faba5efa3f1e5ac7
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2025-02-26 10:28:46 +0330

    style: re-arrange code styles and imports

--- lib/src/features/finish/presentation/screen/finish_page.dart
+++ lib/src/features/finish/presentation/screen/finish_page.dart
@@ -1,17 +1,13 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:go_router/go_router.dart';
+import 'package:gui/src/core/common/widgets/custom_filled_button.dart';
 import 'package:gui/src/core/extensions/context_extensions.dart';
-import 'package:gui/src/core/router/route_name.dart';
-import 'package:gui/src/core/utils/daemon_manager/node_config_data.dart';
-import 'package:gui/src/core/utils/storage_utils.dart';
+import 'package:gui/src/core/utils/gen/assets/assets.gen.dart';
+import 'package:gui/src/core/utils/gen/localization/locale_keys.dart';
+import 'package:gui/src/core/utils/string_extension.dart';
 import 'package:gui/src/features/main/language/core/localization_extension.dart';
 import 'package:pactus_gui_widgetbook/app_styles.dart';
-import '../../../../core/common/widgets/custom_filled_button.dart';
-import '../../../../core/utils/gen/assets/assets.gen.dart';
-import '../../../../core/utils/gen/localization/locale_keys.dart';
-import '../../../../core/utils/string_extension.dart';
 
 class FinishPage extends StatefulWidget {
   const FinishPage({super.key});
