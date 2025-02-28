9f677cf8ce713c45dab7c14b986e60f46f00039a
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2025-02-28 23:44:53 +0330

    fix(features:splash): check correct password by `pactus-wallet`

--- lib/src/features/password/presentation/screen/unblock_password_screen.dart
+++ lib/src/features/password/presentation/screen/unblock_password_screen.dart
@@ -1,12 +1,16 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
-import 'package:gui/src/core/common/widgets/custom_filled_button.dart';
 import 'package:gui/src/core/common/widgets/custom_password_widget.dart';
 import 'package:gui/src/core/router/route_name.dart';
+import 'package:gui/src/core/utils/daemon_manager/bloc/cli_command.dart';
+import 'package:gui/src/core/utils/daemon_manager/bloc/daemon_cubit.dart';
+import 'package:gui/src/core/utils/daemon_manager/bloc/daemon_state.dart';
 import 'package:gui/src/core/utils/gen/assets/assets.gen.dart';
 import 'package:gui/src/core/utils/gen/localization/locale_keys.dart';
 import 'package:gui/src/features/main/language/core/localization_extension.dart';
+import 'package:gui/src/features/validator_config/core/utils/methods/show_fluent_alert_method.dart';
 import 'package:pactus_gui_widgetbook/app_styles.dart';
 import '../../../../core/common/colors/app_colors.dart';
 
@@ -14,14 +18,6 @@ class UnblockPasswordScreen extends StatefulWidget {
   const UnblockPasswordScreen({super.key, required this.fromRegistrationRoute});
   final bool fromRegistrationRoute;
 
-  @override
-  State<UnblockPasswordScreen> createState() => _UnblockPasswordScreenState();
-}
-
-class _UnblockPasswordScreenState extends State<UnblockPasswordScreen> {
-  String? _errorMessage;
-  String _passwordValue = '';
-  // Constants for layout
   static const double _maxContentWidth = 400;
   static const double _lockIconSize = 406;
   static const double _lockIconContainerSize = 364;
@@ -30,8 +26,20 @@ class _UnblockPasswordScreenState extends State<UnblockPasswordScreen> {
   static const double _spacingLarge = 20;
 
   @override
+  State<UnblockPasswordScreen> createState() => _UnblockPasswordScreenState();
+}
+
+class _UnblockPasswordScreenState extends State<UnblockPasswordScreen> {
+  final ValueNotifier<String> passwordNotifier = ValueNotifier<String>('');
+
+  @override
+  void dispose() {
+    passwordNotifier.dispose(); // Prevents memory leaks
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // If password is not set, show loading indicator
     final theme = FluentTheme.of(context);
     final isDark = theme.brightness == Brightness.dark;
     final colors = AppTheme.of(context).extension<DarkPallet>()!;
@@ -47,22 +55,40 @@ class _UnblockPasswordScreenState extends State<UnblockPasswordScreen> {
       ),
       content: Center(
         child: Container(
-          constraints: const BoxConstraints(maxWidth: _maxContentWidth),
+          constraints: const BoxConstraints(
+            maxWidth: UnblockPasswordScreen._maxContentWidth,
+          ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               // Lock Icon Container
-              _buildLockIcon(isDark),
-              const SizedBox(height: _spacingMedium),
+              Container(
+                width: UnblockPasswordScreen._lockIconSize,
+                height: UnblockPasswordScreen._lockIconContainerSize,
+                decoration: BoxDecoration(
+                  color: AppTheme.of(context).scaffoldBackgroundColor,
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: SvgPicture.asset(
+                    isDark
+                        ? Assets.images.masterPasswordDark
+                        : Assets.images.masterPasswordLight,
+                    width: UnblockPasswordScreen._lockIconSize,
+                    height: UnblockPasswordScreen._lockIconContainerSize,
+                  ),
+                ),
+              ),
+              const SizedBox(height: UnblockPasswordScreen._spacingMedium),
 
               // Small Lock Icon
               Assets.icons.lock.image(
-                width: _smallLockIconSize,
-                height: _smallLockIconSize,
+                width: UnblockPasswordScreen._smallLockIconSize,
+                height: UnblockPasswordScreen._smallLockIconSize,
                 fit: BoxFit.contain,
                 color: isDark ? AppColors.primaryLight : AppColors.primaryDark,
               ),
-              const SizedBox(height: _spacingMedium),
+              const SizedBox(height: UnblockPasswordScreen._spacingMedium),
 
               // Instruction Text
               Text(
@@ -71,105 +97,80 @@ class _UnblockPasswordScreenState extends State<UnblockPasswordScreen> {
                   color: colors.dark700,
                 ),
               ),
-              const SizedBox(height: _spacingLarge),
+              const SizedBox(height: UnblockPasswordScreen._spacingLarge),
+
               // Password Field
               CustomPasswordWidget(
                 width: 280,
-                placeholder: '••••••••',
+                placeholder: 'Enter the password ...',
                 onChanged: (value) {
-                  setState(() {
-                    _errorMessage = null;
-                    _passwordValue = value;
-                  });
+                  passwordNotifier.value = value;
                   debugPrint('Password value: $value');
                 },
               ),
 
-              // Error Message
-              if (_errorMessage != null)
-                Padding(
-                  padding: const EdgeInsets.only(top: 8),
-                  child: Text(
-                    _errorMessage!,
-                    style: theme.typography.body!.copyWith(
-                      color: Colors.red,
-                    ),
-                  ),
-                ),
+              const SizedBox(height: UnblockPasswordScreen._spacingLarge),
 
-              const SizedBox(height: _spacingLarge),
+              // Unlock Button with ValueListenableBuilder
+              ValueListenableBuilder<String>(
+                valueListenable: passwordNotifier,
+                builder: (context, password, child) {
+                  return BlocConsumer<DaemonCubit, DaemonState>(
+                    listener: (ctxListener, state) {
+                      if (state is DaemonSuccess) {
+                        final isPasswordCorrect = state.output
+                            .contains('Your wallet password successfully');
+                        if (isPasswordCorrect) {
+                          if (widget.fromRegistrationRoute) {
+                            context.goNamed(AppRoute.dashboard.name);
+                          } else {
+                            context.goNamed(AppRoute.basicDashboard.name);
+                          }
+                        } else {
+                          showFluentAlert(
+                            context,
+                            context.tr(LocaleKeys.incorrect_password),
+                          );
+                        }
+                      }
+                    },
+                    builder: (ctxBuilder, state) {
+                      return SizedBox(
+                        width: 100,
+                        child: FilledButton(
+                          onPressed: password.isNotEmpty
+                              ? () {
+                                  final cliCommand = CliCommand(
+                                    command: './pactus-wallet',
+                                    arguments: ['password', password],
+                                  );
+                                  context
+                                      .read<DaemonCubit>()
+                                      .runPactusDaemon(cliCommand: cliCommand);
 
-              // Unlock Button
-              _buildUnlockButton(),
+                                  debugPrint(
+                                    'Unlocking with password: $password',
+                                  );
+                                }
+                              : null, // Disable button if password is empty
+                          child:
+                              context.read<DaemonCubit>().state is DaemonLoading
+                                  ? SizedBox(
+                                      height: 26,
+                                      width: 26,
+                                      child: ProgressRing(),
+                                    )
+                                  : Text(context.tr(LocaleKeys.unlock_wallet)),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
             ],
           ),
         ),
       ),
     );
-  }
-
-  Widget _buildLockIcon(bool isDark) {
-    return Container(
-      width: _lockIconSize,
-      height: _lockIconContainerSize,
-      decoration: BoxDecoration(
-        color: AppTheme.of(context).scaffoldBackgroundColor,
-        shape: BoxShape.circle,
-      ),
-      child: Center(
-        child: SvgPicture.asset(
-          isDark
-              ? Assets.images.masterPasswordDark
-              : Assets.images.masterPasswordLight,
-          width: _lockIconSize,
-          height: _lockIconContainerSize,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildUnlockButton() {
-    return CustomFilledButton(
-      text: LocaleKeys.unlock_wallet,
-      onPressed: _handleUnlock,
-      style: ButtonStyle(
-        padding: WidgetStateProperty.all<EdgeInsetsDirectional?>(
-          EdgeInsetsDirectional.symmetric(horizontal: 24, vertical: 4),
-        ),
-        backgroundColor: WidgetStateProperty.all(Color(0xFF0066B4)),
-        shape: WidgetStateProperty.all(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(4),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Future<void> _handleUnlock() async {
-    setState(() {
-      _errorMessage = null;
-    });
-    final password = _passwordValue.trim();
-    if (password.isEmpty) {
-      setState(() {
-        _errorMessage = context.tr(LocaleKeys.password_cannot_be_empty);
-      });
-      return;
-    }
-    _navigateToDashboard();
-  }
-
-  void _navigateToDashboard() {
-    if (widget.fromRegistrationRoute) {
-      context.goNamed(AppRoute.dashboard.name);
-    } else {
-      context.goNamed(AppRoute.basicDashboard.name);
-    }
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
   }
 }
