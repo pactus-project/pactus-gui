df9b45efd8a8899db7137cfa940a1c780cd5781b
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2025-10-27 22:21:39 +0330

    fix(transaction): fix missing ui parts in `BondInputForm` component
    - add `Validator Public Key` widget
    - add `Sender` widget
    - add `validatorAddress` widget

--- lib/src/features/transactions/presentation/forms/bond_input_form.dart
+++ lib/src/features/transactions/presentation/forms/bond_input_form.dart
@@ -3,10 +3,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pactus_gui/src/core/common/widgets/form_row_item.dart'
     show FormRowItem;
 import 'package:pactus_gui/src/core/common/widgets/text_input.dart' show TextInputBox;
-import 'package:pactus_gui/src/core/enums/app_enums.dart' show InputFilter, TransactionType;
+import 'package:pactus_gui/src/core/enums/app_enums.dart' show AddressType, InputFilter, TransactionType;
+import 'package:pactus_gui/src/core/utils/daemon_manager/bloc/daemon_manager_bloc.dart' show DaemonManagerBloc;
 import 'package:pactus_gui/src/core/utils/gen/localization/locale_keys.dart';
 import 'package:pactus_gui/src/features/transactions/presentation/blocs/transaction_type_cubit.dart'
     show TransactionTypeCubit;
+import 'package:pactus_gui/src/features/transactions/presentation/widgets/address_combo_box.dart' show AddressComboBox;
 import 'package:pactus_gui/src/features/transactions/presentation/widgets/transaction_type_selector.dart'
     show TransactionTypeSelector;
 import 'package:pactus_gui_widgetbook/app_styles.dart' show AppTheme, DarkPallet;
@@ -15,56 +17,83 @@ class BondInputForm extends StatelessWidget {
   const BondInputForm({super.key});
   @override
   Widget build(BuildContext context) {
-    return Column(
-      spacing: 16,
-      children: [
-        FormRowItem(
-          title: LocaleKeys.transaction_type,
-          inputWidget: TransactionTypeSelector(
-            initialValue: TransactionType.bond,
-            onChanged: (selectedItem) {
-              context.read<TransactionTypeCubit>().selectType(selectedItem!);
-            },
-          ),
-        ),
-        FormRowItem(
-          title: LocaleKeys.memo,
-          inputWidget: TextInputBox(
-            placeholder: LocaleKeys.addNote,
-            onChanged: (result) {},
-            maxLength: 64,
-          ),
-        ),
-        FormRowItem(
-          title: LocaleKeys.amount,
-          isMandatory: true,
-          inputWidget: TextInputBox(
-            inputFilter: InputFilter.numbersWithDecimal,
-            placeholder: LocaleKeys.enterAmount,
-            onChanged: (result) {},
-            suffix: Text(
-              'PAC',
-              style: TextStyle(
-                color: AppTheme.of(context).extension<DarkPallet>()!.contrast,
-              ),
-            ),
-          ),
-        ),
-        FormRowItem(
-          title: LocaleKeys.fee,
-          inputWidget: TextInputBox(
-            inputFilter: InputFilter.numbersWithDecimal,
-            placeholder: LocaleKeys.enterFee,
-            onChanged: (result) {},
-            suffix: Text(
-              'PAC',
-              style: TextStyle(
-                color: AppTheme.of(context).extension<DarkPallet>()!.contrast,
-              ),
-            ),
-          ),
-        ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<DaemonManagerBloc>(create: (_) => DaemonManagerBloc()),
       ],
+      child:  Column(
+        spacing: 16,
+        children: [
+          FormRowItem(
+            title: LocaleKeys.transaction_type,
+            inputWidget: TransactionTypeSelector(
+              initialValue: TransactionType.bond,
+              onChanged: (selectedItem) {
+                context.read<TransactionTypeCubit>().selectType(selectedItem!);
+              },
+            ),
+          ),
+          FormRowItem(
+            title: LocaleKeys.sender,
+            inputWidget: AddressComboBox(
+              addressType: AddressType.wallet,
+              onChanged: (result) {},
+            ),
+          ),
+          FormRowItem(
+            title: LocaleKeys.validatorAddress,
+            isMandatory: true,
+            inputWidget: TextInputBox(
+              placeholder: LocaleKeys.enterValidatorAddress,
+              onChanged: (result) {},
+            ),
+          ),
+          FormRowItem(
+            title: LocaleKeys.validatorPublicKey,
+            inputWidget: TextInputBox(
+              placeholder: LocaleKeys.validatorPublicKeyDescription,
+              onChanged: (result) {},
+            ),
+          ),
+          FormRowItem(
+            title: LocaleKeys.memo,
+            inputWidget: TextInputBox(
+              placeholder: LocaleKeys.addNote,
+              onChanged: (result) {},
+              maxLength: 64,
+            ),
+          ),
+          FormRowItem(
+            title: LocaleKeys.amount,
+            isMandatory: true,
+            inputWidget: TextInputBox(
+              inputFilter: InputFilter.numbersWithDecimal,
+              placeholder: LocaleKeys.enterAmount,
+              onChanged: (result) {},
+              suffix: Text(
+                'PAC',
+                style: TextStyle(
+                  color: AppTheme.of(context).extension<DarkPallet>()!.contrast,
+                ),
+              ),
+            ),
+          ),
+          FormRowItem(
+            title: LocaleKeys.fee,
+            inputWidget: TextInputBox(
+              inputFilter: InputFilter.numbersWithDecimal,
+              placeholder: LocaleKeys.enterFee,
+              onChanged: (result) {},
+              suffix: Text(
+                'PAC',
+                style: TextStyle(
+                  color: AppTheme.of(context).extension<DarkPallet>()!.contrast,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
